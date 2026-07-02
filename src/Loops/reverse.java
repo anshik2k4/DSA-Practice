67801a28de430660db2e5f2134ad2720a473c88d
Anshik Kumar <anshikkumarak@gmail.com>
2026-07-03 00:42:07 +0530

    solved a new problem

--- src/Loops/reverse.java
+++ src/Loops/reverse.java
@@ -4,12 +4,21 @@ import java.util.*;
 public class reverse {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
+        int original=sc.nextInt();
+        int n=original;
+        int rev=0;
         //we have to print reverse of this
         while (n>0){
             int num=n%10;
+            rev = rev * 10 + num;
             n=n/10;
-            System.out.print(num+" ");
+
+        }
+        if(original==rev){
+            System.out.println("the num is palindrome");
+        }
+        else{
+            System.out.println("Not palindrome");
         }
     }
 }
